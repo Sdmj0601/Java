60e68d6c2d720121a6d2fe0a35c9345a9e7e63f0
Sage Magnier <107938802+Sdmj0601@users.noreply.github.com>
2023-03-31 23:53:33 +0100

    Add files via upload

---
+++ HelloUniverse.java
@@ -0,0 +1,14 @@
+public class HelloUniverse {
+    public static void main(String[] args) {
+        System.out.println("Hello Sage");
+        int annee = 2006;
+        int nbrplanete = 9;
+        //annee=2006;
+        //if (annee == 2003)
+        //System.out.println("En " + annee + ", les planetes du systeme solaire etaient au nombre de " + nbrplanete);
+        nbrplanete--;
+                if (annee == 2006)
+                    System.out.println("En " + annee + ", les planetes du systeme solaire etaient au nombre de " + nbrplanete);
+    }
+
+}
